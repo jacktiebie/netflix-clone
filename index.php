af3106ba806962cedc5cd93094d4218c830b397c
Jack Tiebie <jacktiebie45@gmail.com>
2021-11-18 09:43:49 +0100

    Copy paste code verwijderen

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
-<?php
-require_once("includes/config.php");
 
-if(!isset($_SESSION["userLoggedIn"])) {
-    header("Location: register.php");
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wewe</title>
+</head>
+<body>
+    
+</body>
+</html>
