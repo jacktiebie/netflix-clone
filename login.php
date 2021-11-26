e192d45ab82d7cd78d10ee864161e87d23736adb
Jack Tiebie <jacktiebie45@gmail.com>
2021-11-26 09:12:49 +0100

    adding videos and thumbnails

--- login.php
+++ login.php
@@ -18,7 +18,7 @@
                 </div>
 
                 <form method="POST">
-                    <?php echo $account->getError(Constants::$loginFailed); ?>
+                   
                     <input type="text" name="username" placeholder="Username" value="<?php getInputValue("username"); ?>" required>
 
                     <input type="password" name="password" placeholder="Password" required>
