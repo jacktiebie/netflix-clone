56d5c056888c50ba0fd2700a2e62c23731e4709c
Jack Tiebie <jacktiebie45@gmail.com>
2021-12-06 11:49:31 +0100

    Adding correct code

---
+++ includes/classes/PreviewProvider.php
@@ -0,0 +1,16 @@
+<?php
+class PreviewProvider {
+
+    private $con, $username;
+
+    public function __construct($con, $username) {
+        $this->con = $con;
+        $this->username = $username;
+    }
+
+    public function createPreviewVideo() {
+        echo "hello";
+    }
+
+}
+?>
